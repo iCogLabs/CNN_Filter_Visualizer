ce741c417aa7eda18095ab50399a9e825550b65d
Henok <henoktilaye7@gmail.com>
2019-07-25 11:37:11 +0300

    removed untracked files

---
+++ setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name = "Visualizing-CNNS",
+    version = "1.0",
+    py_modules = ["CNNFilterVisualizer",],
+    packages=setuptools.find_packages(),
+    # Metadata
+
+    author = "Henok Tilaye",
+    author_email = "henoktilaye7@gmail.com",
+    description = "A modle for visualizing CNN filter activations from gradient values with guided backpropagation",
+    long_description_content_type = "text/markdown",
+    long_description = long_description,
+    license = "Public_domain",
+    keywords = "cnn layer activation visualizations",
+)
